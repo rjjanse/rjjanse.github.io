faccc5de231161290e66fa0d3f8061e6615ca3da
Roemer <98459937+rjjanse@users.noreply.github.com>
2022-12-23 23:07:50 +0100

    Add files via upload

---
+++ background.js
@@ -0,0 +1,6 @@
+// Code from: https://stackoverflow.com/questions/27027480/choosing-a-random-background-html-css
+// Define images
+var images = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+
+// Set background
+document.getElementsByClassName("home")[0].style.backgroundImage = "url(https://github.com/rjjanse/rjjanse.github.io/blob/main/images/landing" + images[Math.floor(Math.random() * images.length)] + ".jpeg?raw=true)";
---
+++ modal.js
@@ -0,0 +1,171 @@
+// van Wijk 2022 Med Teach
+// Get the modal
+var modalwijk2022mt = document.getElementById("wijk-2022-mt");
+
+// Get the button that opens the modal
+var btnwijk2022mt = document.getElementById("wijk-2022-mt-button");
+
+// Get the button that closes the modal
+var spanwijk2022mt = document.getElementById("close-button");
+
+// When the user clicks on the button, open the modal
+btnwijk2022mt.onclick = function() {
+    modalwijk2022mt.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+spanwijk2022mt.onclick = function() {
+    modalwijk2022mt.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modalwijk2022mt) {
+    modalwijk2022mt.style.display = "none";
+  }
+} 
+
+//-----------------------------------------------
+
+// Janse 2022 EJHF
+// Get the modal
+var modaljanse2022ejhf = document.getElementById("janse-2022-ejhf");
+
+// Get the button that opens the modal
+var btnjanse2022ejhf = document.getElementById("janse-2022-ejhf-button");
+
+// Get the button that closes the modal
+var spanjanse2022ejhf = document.getElementById("close-button");
+
+// When the user clicks on the button, open the modal
+btnjanse2022ejhf.onclick = function() {
+    modaljanse2022ejhf.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+spanjanse2022ejhf.onclick = function() {
+    modaljanse2022ejhf.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modaljanse2022ejhf) {
+    modaljanse2022ejhf.style.display = "none";
+  }
+} 
+
+//-----------------------------------------------
+
+// Janse 2022 CKJ
+// Get the modal
+var modaljanse2022ckj = document.getElementById("janse-2022-ckj");
+
+// Get the button that opens the modal
+var btnjanse2022ckj = document.getElementById("janse-2022-ckj-button");
+
+// Get the button that closes the modal
+var spanjanse2022ckj = document.getElementById("close-button");
+
+// When the user clicks on the button, open the modal
+btnjanse2022ckjtn.onclick = function() {
+    modaljanse2022ckj.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+spanjanse2022ckj.onclick = function() {
+    modaljanse2022ckj.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modaljanse2022ckj) {
+    modaljanse2022ckj.style.display = "none";
+  }
+} 
+
+//-----------------------------------------------
+
+// Fu 2022 Int J Cardiol
+// Get the modal
+var modalfu2022ijc = document.getElementById("fu-2022-ijc");
+
+// Get the button that opens the modal
+var btnfu2022ijc = document.getElementById("fu-2022-ijc-button");
+
+// Get the button that closes the modal
+var spanfu2022ijc = document.getElementById("close-button");
+
+// When the user clicks on the button, open the modal
+btnfu2022ijc.onclick = function() {
+    modalfu2022ijc.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+spanfu2022ijc.onclick = function() {
+    modalfu2022ijc.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modalfu2022ijc) {
+    modalfu2022ijc.style.display = "none";
+  }
+} 
+
+//-----------------------------------------------
+
+// Janse 2021 CKJ
+// Get the modal
+var modaljanse2021ckj = document.getElementById("janse-2021-ckj");
+
+// Get the button that opens the modal
+var btnjanse2021ckj = document.getElementById("janse-2021-ckj-button");
+
+// Get the button that closes the modal
+var spanjanse202ckj = document.getElementById("close-button");
+
+// When the user clicks on the button, open the modal
+btnjanse2021ckj.onclick = function() {
+    modaljanse2021ckj.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+spanjanse202ckj.onclick = function() {
+    modaljanse2021ckj.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modaljanse2021ckj) {
+    modaljanse2021ckj.style.display = "none";
+  }
+} 
+
+//-----------------------------------------------
+
+// Fu 2020 CKJ
+// Get the modal
+var modalfu2020ckj = document.getElementById("fu-2020-ckj");
+
+// Get the button that opens the modal
+var btnfu2020ckj = document.getElementById("fu-2020-ckj-button");
+
+// Get the button that closes the modal
+var spanfu2022ckj = document.getElementById("close-button");
+
+// When the user clicks on the button, open the modal
+btnfu2020ckj.onclick = function() {
+    modalfu2020ckj.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+spanfu2022ckj.onclick = function() {
+    modalfu2020ckj.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modalfu2020ckj) {
+    modalfu2020ckj.style.display = "none";
+  }
+} 
