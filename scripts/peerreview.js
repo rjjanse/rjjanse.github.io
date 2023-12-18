3c791e29ce03dba6a51f9585c1ee3068d9883eba
Roemer <98459937+rjjanse@users.noreply.github.com>
2023-12-18 10:51:33 +0100

    updated content

---
+++ scripts/peerreview.js
@@ -0,0 +1,56 @@
+// From: https://developers.google.com/chart/interactive/docs/gallery/histogram#overview
+// Load packages
+google.charts.load("current", {packages:["corechart"]});
+google.charts.setOnLoadCallback(drawChart);
+
+// Function for making chart
+function drawChart() {
+  // Data
+  var data = google.visualization.arrayToDataTable([
+    ['Year', 'Number', {role: 'tooltip'}],
+    ["2022", 4, 'AJKD, CKJ, NDT, NTvG'],
+    ["2023", 6, 'BMJ Open, CKJ, JASN, Kidney Int, NDT']
+  ]);
+
+  // Chart options
+  var options = {
+    legend: {
+      position: 'none'
+    },
+    colors: ['#C9CBA3'],
+    backgroundColor: "transparent",
+    chartArea: {
+      backgroundColor: "transparent"
+    },
+    bar: {
+      groupWidth: "95%",
+    },
+    hAxis: {
+      title: "Nr. of papers peer reviewed",
+      titleTextStyle: {
+        color: "#FFE1A8"
+      },
+      textStyle: {
+        color: "#FFE1A8"
+      },
+      gridlines: {
+        color: "#FFE1A8"
+      }
+    },
+    vAxis: {
+      title: "",
+      textStyle: {
+        color: "#FFE1A8"
+      },
+      minValue: 0,
+      gridlines: {
+        color: "#FFE1A8"
+      }
+    },
+    orientation: "vertical" // Flip the bar chart
+  };
+
+  // Draw chart
+  var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+  chart.draw(data, options);
+}
--- scripts/theme.js
+++ scripts/theme.js
@@ -1,3 +1,6 @@
+// Load packages
+google.charts.load("current", {packages:["corechart"]});
+
 // Get theme button
 var theme_btn = document.getElementById("theme");
 
@@ -6,6 +9,9 @@ var style_link = document.getElementById("stylesheet");
 
 // Get all elements with a themed image
 var themed_imgs = document.querySelectorAll(".themed");
+
+// Get script for peer review chart
+//var newChart = new google.visualization.BarChart(document.getElementById("chart_div"));
 
 // Observe button click
 theme_btn.addEventListener("click", (e) => {
@@ -22,11 +28,90 @@ theme_btn.addEventListener("click", (e) => {
     style_link.setAttribute("href", new_style);
 
     // Change themed images
-    for (var i = 0; i < themed_imgs.length; i++) {
+    /*for (var i = 0; i < themed_imgs.length; i++) {
         // Set to corresponding source
         if (new_style.includes("dark")) themed_imgs[i].setAttribute("src", themed_imgs[i].getAttribute("src").replace("light", "dark"));
 
         // Set to corresponding source
         else themed_imgs[i].setAttribute("src", themed_imgs[i].getAttribute("src").replace("dark", "light"));
     }
+
+    // If dark, define new light options
+    if (current_style.includes("dark")) var options = {
+        legend: {
+            position: 'none'
+        },
+        colors: ['#B3C186'],
+        backgroundColor: "transparent",
+        chartArea: {
+          backgroundColor: "transparent"
+        },
+        bar: {
+          groupWidth: "95%",
+        },
+        hAxis: {
+          title: "Nr. of papers peer reviewed",
+          titleTextStyle: {
+            color: "#CA8F53"
+          },
+          textStyle: {
+            color: "#CA8F53"
+          },
+          gridlines: {
+            color: "#CA8F53"
+          }
+        },
+        vAxis: {
+          title: "",
+          textStyle: {
+            color: "#CA8F53"
+          },
+          minValue: 0,
+          gridlines: {
+            color: "#CA8F53"
+          }
+        },
+        orientation: "vertical" // Flip the bar chart
+    };
+
+    // Otherwise, set dark options
+    else options = {
+        legend: {
+          position: 'none'
+        },
+        colors: ['#C9CBA3'],
+        backgroundColor: "transparent",
+        chartArea: {
+          backgroundColor: "transparent"
+        },
+        bar: {
+          groupWidth: "95%",
+        },
+        hAxis: {
+          title: "Nr. of papers peer reviewed",
+          titleTextStyle: {
+            color: "#FFE1A8"
+          },
+          textStyle: {
+            color: "#FFE1A8"
+          },
+          gridlines: {
+            color: "#FFE1A8"
+          }
+        },
+        vAxis: {
+          title: "",
+          textStyle: {
+            color: "#FFE1A8"
+          },
+          minValue: 0,
+          gridlines: {
+            color: "#FFE1A8"
+          }
+        },
+        orientation: "vertical" // Flip the bar chart
+    };
+
+    // Redraw the chart with updated options
+    newChart.draw(newChart.getDataTable(), google.visualization.BarChart.convertOptions(options));*/
 })
