8ab056ac997840fe5abeb16380a95828b56da222
Roemer <98459937+rjjanse@users.noreply.github.com>
2023-12-25 18:27:33 +0100

    added personal content

---
+++ scripts/climbing.js
@@ -0,0 +1,46 @@
+// Get img
+var img = document.getElementById("climb-image");
+
+// Array of possible images
+var images = ["germany.png", "toprope.gif", "france.png", "boulder.gif"];
+
+// Left button
+var left = document.getElementById("interest-img-scroller-left");
+
+// Right button
+var right = document.getElementById("interest-img-scroller-right");
+
+// Start index is 0
+var index = 0
+
+// Listen for left button click
+left.addEventListener("click", (e) => {
+    // If index is 0, go back to 3
+    if (index == 0) {
+        index = 3
+    } else {
+        index -= 1
+    }
+
+    // Get new image
+    var new_image = images[index]
+
+    // Set src of img to new image
+    img.setAttribute("src", "images2/interests/climbing/" + new_image)
+})
+
+// Listen for right button click
+right.addEventListener("click", (e) => {
+    // If index is 0, go back to 3
+    if (index == 3) {
+        index = 0
+    } else {
+        index += 1
+    }
+
+    // Get new image
+    var new_image = images[index]
+
+    // Set src of img to new image
+    img.setAttribute("src", "images2/interests/climbing/" + new_image)
+})
---
+++ scripts/interests.js
@@ -0,0 +1,24 @@
+// Get the button that opens the publication
+var btn = document.querySelectorAll(".interest-button");
+
+// All page publications
+var pubs = document.querySelectorAll(".interest");
+
+// When the user clicks the button, open the modal
+for (var i = 0; i < btn.length; i++) {
+    // Check if button is clicked
+    btn[i].addEventListener("click", (e) => {
+        // Get publication based on button name
+        var pub = document.getElementById(e.target.getAttribute("name"));
+
+        // Set all publications to invisible
+        for (var p = 0; p < pubs.length; p++) {
+            // Set publication to not visible
+            pubs[p].style.display = "none";
+        }
+
+        // Set publication visible
+        pub.style.display = "block";
+ });
+}
+ 
