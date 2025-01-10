95424d18df30393b7ec24171152fd19c3795c603
Roemer <98459937+rjjanse@users.noreply.github.com>
2025-01-10 14:43:49 +0100

    updated peer review

--- scripts/peerreview.js
+++ scripts/peerreview.js
@@ -7,10 +7,11 @@ google.charts.setOnLoadCallback(drawChart);
 function drawChart() {
   // Data
   var data = google.visualization.arrayToDataTable([
-    ['Year', 'Number', {role: 'tooltip'}],
-    ["2022", 4, "AJKD, CKJ, NDT, NTvG"],
-    ["2023", 6, "BMJ Open, CKJ, JASN, Kidney Int, NDT"],
-    ["2024", 11, "BMC Medical Educ, CKJ, Health Sci Rep, J Nephrol, Kidney Med, Kidney360, Lancet Healthy Longev, NDT, Sci Rep"]
+    ['Year', 'Number'],
+    ["2022", 4],
+    ["2023", 6],
+    ["2024", 11]
+    ["2025", 3]
   ]);
 
   // Chart options
